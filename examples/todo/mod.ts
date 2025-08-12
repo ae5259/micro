91a7b9f55a6a9ee2ea2ad4b79f6c42637fb2ee34
ae5259 <isakulovdev@gmail.com>
2025-08-12 16:36:00 +0500

    fix: method on example

--- examples/todo/mod.ts
+++ examples/todo/mod.ts
@@ -4,11 +4,11 @@ import { MicroRequest, MicroResponse } from "../../src/types.ts";
 const app = new Micro();
 
 app.get("/:id/", (req: MicroRequest, res: MicroResponse) => {
-  return res.jsonify(req.params);
+  return res.json(req.params);
 });
 
 app.get("/name/:name/", (req: MicroRequest, res: MicroResponse) => {
-  return res.jsonify(req.params);
+  return res.json(req.params);
 });
 
 app.run(3000);
